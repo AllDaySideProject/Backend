31fc2c426b6e86de125af5706fa41f16ea954e7c
oroi2009 <oroi2009@gmail.com>
2025-08-10 18:56:42 +0900

    ♻️  refactor : member 도메인 삭제

--- src/main/java/com/example/Centralthon/domain/member/exception/MemberAlreadyExistException.java
+++
@@ -1,8 +0,0 @@
-package com.example.Centralthon.domain.member.exception;
-
-import com.example.Centralthon.global.exception.BaseException;
-import com.example.Centralthon.global.response.code.BaseResponseCode;
-
-public class MemberAlreadyExistException extends BaseException {
-    public MemberAlreadyExistException() {super(MemberErrorCode.MEMBER_ALREADY_EXIST);}
-}
--- src/main/java/com/example/Centralthon/domain/member/exception/MemberNotFoundException.java
+++
@@ -1,7 +0,0 @@
-package com.example.Centralthon.domain.member.exception;
-
-import com.example.Centralthon.global.exception.BaseException;
-
-public class MemberNotFoundException extends BaseException {
-    public MemberNotFoundException() {super(MemberErrorCode.MEMBER_NOT_FOUND);}
-}
