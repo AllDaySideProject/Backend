08f9bdc61a8904fd4c6465c4778e7e005e0fe5f2
oroi2009 <oroi2009@gmail.com>
2025-08-12 21:11:46 +0900

    ♻️  refactor : Res 네이밍 수정

--- src/main/java/com/example/Centralthon/domain/menu/service/MenuService.java
+++ src/main/java/com/example/Centralthon/domain/menu/service/MenuService.java
@@ -1,12 +1,12 @@
 package com.example.Centralthon.domain.menu.service;
 
 import com.example.Centralthon.domain.menu.web.dto.NearbyMenusRes;
-import com.example.Centralthon.domain.menu.web.dto.StoresByMenu;
+import com.example.Centralthon.domain.menu.web.dto.StoresByMenuRes;
 import jakarta.validation.constraints.NotNull;
 import java.util.List;
 
 public interface MenuService {
     List<NearbyMenusRes> nearbyMenus(double latitude, double longitude);
 
-    List<StoresByMenu> storesByMenu(String name, double lat, double lng);
+    List<StoresByMenuRes> storesByMenu(String name, double lat, double lng);
 }
