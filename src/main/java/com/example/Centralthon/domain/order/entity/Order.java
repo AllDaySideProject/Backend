b381b66f912e33150c44b46b9369eb4e404a3893
frombunny <rucy011121@gmail.com>
2025-08-14 14:25:52 +0900

    :sparkles: feat: Order 엔티티 작성

---
+++ src/main/java/com/example/Centralthon/domain/order/entity/Order.java
@@ -0,0 +1,29 @@
+package com.example.Centralthon.domain.order.entity;
+
+import com.example.Centralthon.domain.menu.entity.Menu;
+import com.example.Centralthon.global.entity.BaseEntity;
+import jakarta.persistence.*;
+import lombok.*;
+
+@Getter
+@Setter
+@Builder
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+@AllArgsConstructor(access = AccessLevel.PROTECTED)
+@Entity
+@Table(name = "order")
+public class Order extends BaseEntity {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name="order_id")
+    private Long id;
+
+    @Column(nullable = false)
+    private String pickUpCode;
+
+    @Column(nullable = false)
+    private int price;
+
+    @Column(nullable = false)
+    private Boolean isPickedUp;
+}
