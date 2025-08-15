f07d7d9933dbffdf118ab3fecd0b27bce8be2677
oroi2009 <oroi2009@gmail.com>
2025-08-15 09:56:58 +0900

    ♻️  refactor : 쿼리문 수정 및 파라미터 어노테이션 추가

--- src/main/java/com/example/Centralthon/domain/store/repository/StoreRepository.java
+++ src/main/java/com/example/Centralthon/domain/store/repository/StoreRepository.java
@@ -4,6 +4,7 @@ import com.example.Centralthon.domain.menu.entity.Menu;
 import com.example.Centralthon.domain.store.entity.Store;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 
 import java.time.LocalDateTime;
@@ -11,20 +12,20 @@ import java.util.List;
 
 @Repository
 public interface StoreRepository extends JpaRepository<Store, Long> {
-    @Query(value = "SELECT s.* FROM stores s " +
-            "WHERE s.latitude BETWEEN :minLat AND :maxLat AND s.longitude BETWEEN :minLng AND :maxLng " +
-            "AND ST_Distance_Sphere(POINT(s.longitude, s.latitude), POINT(:lng, :lat)) <= 2000 " +
-            "AND EXISTS ( " +
-            "    SELECT 1 FROM menus m " +
-            "    WHERE m.store_id = s.store_id AND m.quantity > 0 AND m.deadline > NOW())"
-            , nativeQuery = true)
+    @Query(value =  """ 
+            SELECT DISTINCT s.* FROM stores s
+            JOIN menus m ON m.store_id = s.store_id
+            WHERE s.latitude BETWEEN :minLat AND :maxLat AND s.longitude BETWEEN :minLng AND :maxLng
+            AND ST_Distance_Sphere(POINT(s.longitude, s.latitude), POINT(:lng, :lat)) <= 2000
+            AND m.quantity > 0 AND m.deadline > :now
+            """, nativeQuery = true)
     List<Store> findNearbyStores(
-            double lat,
-            double lng,
-            LocalDateTime now,
-            double minLat,
-            double maxLat,
-            double minLng,
-            double maxLng
+            @Param("lat") double lat,
+            @Param("lng") double lng,
+            @Param("now") LocalDateTime now,
+            @Param("minLat") double minLat,
+            @Param("maxLat") double maxLat,
+            @Param("minLng") double minLng,
+            @Param("maxLng") double maxLng
     );
 }
