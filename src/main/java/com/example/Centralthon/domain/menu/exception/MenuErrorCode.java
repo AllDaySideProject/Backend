0d56a0ebbf38c542d961e8a88f2268283fe28f5e
frombunny <rucy011121@gmail.com>
2025-08-16 01:57:03 +0900

    :recycle: refactor: MenuErrorCode 코드 변경

--- src/main/java/com/example/Centralthon/domain/menu/exception/MenuErrorCode.java
+++ src/main/java/com/example/Centralthon/domain/menu/exception/MenuErrorCode.java
@@ -7,9 +7,9 @@ import lombok.Getter;
 @Getter
 @AllArgsConstructor
 public enum MenuErrorCode implements BaseResponseCode {
-    MENU_NOT_FOUND("MENU_NOT_FOUND_404_1",404,"존재하지 않는 메뉴 입니다."),
-    MENU_EXPIRED("MENU_EXPIRED_400_1", 400, "판매 마감된 메뉴입니다."),
-    MENU_OUT_OF_STOCK("MENU_OUT_OF_STOCK_400_2", 400, "재고가 부족합니다.");
+    MENU_EXPIRED("MENU_400_1", 400, "판매 마감된 메뉴입니다."),
+    MENU_OUT_OF_STOCK("MENU_400_2", 400, "재고가 부족합니다."),
+    MENU_NOT_FOUND("MENU_404_1",404,"존재하지 않는 메뉴 입니다.");
 
     private final String code;
     private final int httpStatus;
